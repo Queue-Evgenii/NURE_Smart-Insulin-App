74a26afe60ef87ee49526a9a44f3fa0aed77f758
Queue-Yevhenii <yevhenii.dolhyi@gmail.com>
2026-04-26 01:39:13 +0300

    FIX: refresh token not called when access token expired

--- smart-insulin-backend/src/main/kotlin/ua/nure/smartinsulinbackend/config/SecuritiConfig.kt
+++ smart-insulin-backend/src/main/kotlin/ua/nure/smartinsulinbackend/config/SecuritiConfig.kt
@@ -2,6 +2,7 @@ package ua.nure.smartinsulinbackend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -9,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
@@ -31,6 +33,7 @@ class SecurityConfig(private val jwtAuthFilter: JwtAuthenticationFilter) {
                 auth.anyRequest().authenticated()
             }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .exceptionHandling { it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) }
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
